fbc4ca0f10ae8e5ac7ad151065cea8f7ea92bb9a
Ludovic BENIMELIS <l.benimelis@alfun.fr>
2021-04-07 22:28:34 +0200

    v2

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
-### Création du ressource group HUB ###
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.namerg}-rg"
-  location = var.location
 
-  tags = {
-    environment = var.env
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = merge({ "Name" = format("%s", var.resource_group_name) }, var.tags, )
 }
 
 
+#-------------------------------------
+# Lock - Default is "true"
+#-------------------------------------
+
+
+resource "azurerm_management_lock" "Lock_rg" {
+count    = var.create_management_lock != false ? 1 : 0
+  name       = "Lock-${var.namerg}"
+  scope      = azurerm_resource_group.rg.id
+  lock_level = "CanNotDelete"
+  notes      = "This Resource Group can't be Deleted"
+}
 
--- variables.tf
+++ variables.tf
@@ -1,11 +1,18 @@
-variable "namerg" {
-  description = "namerg"
-}
-
-variable "env" {
-  description = "Env"
+variable "resource_group_name" {
+  description = "resource_group_name"
 }
 
 variable "location" {
   description = "location"
+}
+
+variable "create_management_lock" {
+  description = "Controls if create lock for RG"
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
 }
